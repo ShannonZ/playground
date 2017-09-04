055cb65842c057576f807f251be1e041b9b9fab1
ShannonZhou <myisland@126.com>
2017-09-04 10:04:34 +0800

    SF Custom Editor Quirk Representation
    Custom Editor cannot update data

---
+++ Playground/Playground/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Playground
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public DispatcherTimer Timer;
+        public Model m;
+        public Model1 m1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            m = new Model();
+            m1 = new Model1();
+            DataContext = m;
+            sf.SelectedObject = m;
+            sf1.SelectedObject = m1;
+
+            Timer = new DispatcherTimer(DispatcherPriority.Render);
+            Timer.Tick += new EventHandler(Timer_Tick);
+            Timer.Interval = new System.TimeSpan(0, 0, 0, 0, 10);
+            Timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Debug.WriteLine("m.SF="+m.SF.ToString()+"  m1.SF="+m1.SF.ToString());
+        }
+    }
+}
---
+++ Playground/Playground/Model.cs
@@ -0,0 +1,47 @@
+﻿using SoftFluent.Windows;
+using System;
+using System.ComponentModel;
+
+namespace Playground
+{
+    public class Model: AutoObject,INotifyPropertyChanged
+    {
+
+        [Category("Cumstumized Editor")]
+        [DisplayName("using SF AutoObject")]
+        [PropertyGridOptions(SortOrder = 2, EditorDataTemplateResourceKey = "MyEditor")]
+        public double SF
+        {
+            get { return GetProperty<double>(); }
+            set {
+                SetProperty(value);
+            }
+        }
+
+       public Model()
+        {
+            SF = 12.345;
+        }
+    }
+
+    public class Model1 : AutoObject, INotifyPropertyChanged
+    {
+
+        [Category("Default Editor")]
+        [DisplayName("using SF AutoObject")]
+        [PropertyGridOptions(SortOrder = 2)]
+        public double SF
+        {
+            get { return GetProperty<double>(); }
+            set
+            {
+                SetProperty(value);
+            }
+        }
+
+        public Model1()
+        {
+            SF = 12.345;
+        }
+    }
+}
