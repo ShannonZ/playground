9a1788df8055dfc0f60811fa9b230a9352b41850
ShannonZ <myisland@126.com>
2019-03-15 13:46:17 +0800

    render to texture

---
+++ RayCasting3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayCasting3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RenderWindow win = new RenderWindow(400, 400, "RayCasting"))
+            {
+                win.Run(60);
+            }
+        }
+    }
+}
