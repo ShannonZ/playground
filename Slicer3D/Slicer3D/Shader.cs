9ff4ca960e6e9dc620e9a565e4639ad647a0c44f
ShannonZ <myisland@126.com>
2020-11-23 15:44:39 +0800

    why no cube

--- Slicer3D/Slicer3D/Shader.cs
+++ Slicer3D/Slicer3D/Shader.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 
-namespace LearnOpenTK.Common
+namespace Slicer3D
 {
     // A simple class meant to help create shaders.
     public class Shader
