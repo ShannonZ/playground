b03c35d86c5e3887013eb25f7fd8ed9a8589b2cb
ShannonZ <myisland@126.com>
2024-08-27 10:39:50 +0800

    memleak

--- repo/App.xaml.cs
+++ repo/App.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace Client
 {
     public partial class App : Application
     {
-
+        protected override void OnStartup(StartupEventArgs e)
+        {
+           //RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
+        }
     }
 }
